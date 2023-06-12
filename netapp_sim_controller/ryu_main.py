88d32fd85b617509332ea6ed4b8acaf6b70002ee
Youcef OUARAB <hy_ouarab@esi.dz>
2023-06-12 11:02:49 +0100

    added logging (temp)

--- netapp_sim_controller/ryu_main.py
+++ netapp_sim_controller/ryu_main.py
@@ -32,18 +32,20 @@ class RyuMain(RyuApp):
         self.delay_monitor = kwargs[DELAY_MONITOR]
         self.metrics = kwargs[METRICS]
 
-        #spawn(self._test)
+        spawn(self._test)
 
     def _test(self):
         from pprint import pprint
         i = 0
         while True:
-            sleep(1)
+            sleep(2)
             i += 1
             # print(i)
             # print()
-            #pprint(self.simple_arp.arp_table)
-            #print()
+            print('### ARP TABLE ###')
+            pprint(self.simple_arp.arp_table)
+            print('#################')
+            print()
             #pprint(self.simple_arp._reverse_arp_table)
             #print()
             #pprint(self.simple_arp._in_ports)
@@ -54,13 +56,20 @@ class RyuMain(RyuApp):
             #print()
             #pprint(self.network_monitor.port_speed)
             #print()
-            #pprint(self.network_monitor.free_bandwidth)
-            #print()
-            #for src in self.network_monitor.loss_rate:
-            #    for dst in self.network_monitor.loss_rate[src]:
-            #        loss = self.network_monitor.loss_rate[src][dst]
-            #        print(src, '-->', dst, round(loss * 100, 2), '%')
-            #print()
+            print('### FREE BANDWIDTH ###')
+            for dpid, ports in self.network_monitor.free_bandwidth.items():
+                for port, (bw_up, bw_down) in ports.items():
+                    print('< switch %d, port %d > : UP %.2f Mbps | '
+                          'DOWN %.2f Mbps' % (dpid, port, bw_up, bw_down))
+            print('######################')
+            print()
+            print('### LOSS RATE ###')
+            for src in self.network_monitor.loss_rate:
+                for dst in self.network_monitor.loss_rate[src]:
+                    loss = self.network_monitor.loss_rate[src][dst]
+                    print(src, '-->', dst, ':', round(loss * 100, 2), '%')
+            print('#################')
+            print()
             #for src in self.network_delay_detector.lldp_latency:
             #    for dst in self.network_delay_detector.lldp_latency[src]:
             #        lat = self.network_delay_detector.lldp_latency[src][dst]
@@ -70,25 +79,36 @@ class RyuMain(RyuApp):
             #    lat = self.network_delay_detector.echo_latency[dpid]
             #    print(dpid, '<-->', 'ctrl', round(lat * 1000, 2), 'ms')
             #print()
-            #for src in self.network_delay_detector.delay:
-            #    for dst in self.network_delay_detector.delay[src]:
-            #        lat = self.network_delay_detector.delay[src][dst]
-            #        print(src, '-->', dst, round(lat * 1000, 2), 'ms')
-            #print()
+            print('### SWITCH DELAY ###')
+            for src in self.network_delay_detector.delay:
+                for dst in self.network_delay_detector.delay[src]:
+                    lat = self.network_delay_detector.delay[src][dst]
+                    print(src, '-->', dst, ':', round(lat * 1000, 2), 'ms')
+            print('####################')
+            print()
             #for src, dst in self.network_delay_detector._delay_history:
             #    delays = self.network_delay_detector._delay_history[(src,dst)]
             #    print(src, '-->', dst, end='')
             #    pprint([round(delay * 1000, 2) for delay in delays])
             #print()
-            #for src in self.network_delay_detector.jitter:
-            #    for dst in self.network_delay_detector.jitter[src]:
-            #        jitter = self.network_delay_detector.jitter[src][dst]
-            #        print(src, '-->', dst, round(jitter * 1000, 2), 'ms')
-            #print()
+            print('### SWITCH JITTER ###')
+            for src in self.network_delay_detector.jitter:
+                for dst in self.network_delay_detector.jitter[src]:
+                    jitter = self.network_delay_detector.jitter[src][dst]
+                    print(src, '-->', dst, ':', round(jitter * 1000, 2), 'ms')
+            print('#####################')
+            print()
             #for ip, jitters in self.delay_monitor._delay_history.items():
             #    print(ip, ':', end='')
             #    pprint([round(jitter * 1000, 2) for jitter in jitters])
             #print()
-            #for ip, jitter in self.delay_monitor.jitter.items():
-            #    print(ip, ':', round(jitter * 1000, 2), 'ms')
-            #print()
+            print('### HOST DELAY ###')
+            for ip, delay in self.delay_monitor.delay.items():
+                print(ip, '<-> switch :', round(delay * 1000, 2), 'ms')
+            print('##################')
+            print()
+            print('### HOST JITTER ###')
+            for ip, jitter in self.delay_monitor.jitter.items():
+                print(ip, '<-> switch :', round(jitter * 1000, 2), 'ms')
+            print('###################')
+            print()
